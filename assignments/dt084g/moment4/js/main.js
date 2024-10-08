4521e1e4f1ffa368e35cf5bd33d278bf801ed1ab
William Pettersson <wille.pettersson@icloud.com>
2024-10-08 23:23:49 +0200

    Add files

---
+++ assignments/dt084g/moment4/js/main.js
@@ -0,0 +1,67 @@
+"use strict";
+
+// Deklarera variabler
+const inputField = document.getElementById("newtodo");
+const addButton = document.getElementById("newtodobutton");
+const messageField = document.getElementById("message");
+const listSection = document.getElementById("todolist");
+const clearButton = document.getElementById("clearbutton");
+let textPermitted = false;
+
+// Deklarera funktioner
+function readStorage(){
+    const storedKeys = Object.keys(localStorage); // Skapa en array med alla Web Storage nycklar
+
+    storedKeys.reverse().forEach((key) => { // Loopa igenom all data i Web Storage och skapa DOM objekt, .reverse() ser till att ordningen inte ändras
+        const storedData = localStorage.getItem(key);
+        addItem(key, storedData);
+    });
+}
+
+function writeStorage(){
+    if (textPermitted){
+        const id = Date.now(); // Skapa unikt id genom att använda UNIX tid
+        const itemText = inputField.value; // Hämta text från textrutan
+
+        localStorage.setItem(id, itemText); // Spara värdet i Web Storage
+        if (localStorage.getItem(id) === itemText){ // Kollar så att värdet sparades korrekt, lägg i så fall till det i DOM och rensa textrutan
+            inputField.value = "";
+            addItem(id, itemText);
+        } else {
+            messageField.innerHTML = "Lagring misslyckades!"; // Meddela användaren att det inte gick att spara
+        }
+    }
+}
+
+function clearStorage(){
+    localStorage.clear(); // Rensa Web Storage
+    listSection.innerHTML = ""; // Rensa DOM
+}
+
+function checkLength(){
+    const text = inputField.value; // Hämta text från textrutan
+
+    if (text.length < 5){ // Kollar om texten är tillräckligt lång nog att spara, skriv annars ett meddelande till användaren
+        messageField.innerHTML = "Minst antal tecken: 5";
+        textPermitted = false;
+    } else {
+        messageField.innerHTML = "";
+        textPermitted = true;
+    }
+}
+
+function addItem(id, text){ // Skapa en ny <article> i DOM med anteckningen
+    listSection.innerHTML += "<article id='" + id + "' onclick='deleteItem(" + id + ")'>" + text + "</article>";
+}
+
+function deleteItem(id){
+    document.getElementById(id).remove(); // Ta bort vald anteckning från DOM
+    localStorage.removeItem(id); // Ta bort vald anteckning från Web Storage
+}
+
+// Event handlers
+addEventListener("load", readStorage);
+inputField.addEventListener("keyup", checkLength);
+addButton.addEventListener("click", writeStorage);
+clearButton.addEventListener("click", clearStorage);
+
